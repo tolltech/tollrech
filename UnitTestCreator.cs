922e0280c09b872e9c65e966a1ef31df06a846a6
tolltech <paul@skbkontur.ru>
2016-02-20 22:27:46 +0400

    1.0.0

--- UnitTestCreator.cs
+++ UnitTestCreator.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Application.Progress;
 using JetBrains.ProjectModel;
 using JetBrains.ReSharper.Daemon.CSharp.Errors;
-using JetBrains.ReSharper.Feature.Services.ContextActions;
-using JetBrains.ReSharper.Feature.Services.CSharp.Analyses.Bulbs;
 using JetBrains.ReSharper.Feature.Services.QuickFixes;
 using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Psi.CSharp;
@@ -16,63 +13,6 @@ using JetBrains.Util;
 
 namespace Tollrech
 {
-    [ContextAction(Group = "C#", Name = "UnitTestCreate Action", Description = "Create unit test")]
-    public class UnitTestCreator : ContextActionBase
-    {
-        public ICSharpContextActionDataProvider Provider { get; set; }
-
-        public UnitTestCreator(ICSharpContextActionDataProvider provider)
-        {
-            Provider = provider;
-        }
-
-        public override string Text { get; } = "Create mocked class";
-
-        public override bool IsAvailable(IUserDataHolder cache)
-        {
-            var method = Provider.GetSelectedElement<IMethodDeclaration>();
-            if (method == null)
-                return false;
-
-            var ctor = Provider.GetSelectedElement<IConstructorInitializer>(false);
-            if (ctor == null)
-                return false;
-
-            return true;
-        }
-
-        protected override Action<ITextControl> ExecutePsiTransaction(ISolution solution, IProgressIndicator progress)
-        {
-            ReplaceType();
-
-            return null;
-        }
-
-        private void ReplaceType()
-        {
-            var ctor = Provider.GetSelectedElement<IConstructorInitializer>(false);
-            var type = ctor?.ConstructedType;
-            if (ctor == null || type == null)
-                return;
-
-            var typePresentableName = type.GetPresentableName(CSharpLanguage.Instance);
-
-            var factory = CSharpElementFactory.GetInstance(Provider.PsiModule);
-            var ctorArgs = ctor.ArgumentList;
-
-            foreach (var argument in ctorArgs.Arguments)
-            {
-                var code = $"NewMock<>()";
-
-                var newExpression = factory.CreateExpression(code);
-                argument.SetValue(newExpression);
-            }
-
-            ctor.SetArgumentList(ctorArgs);
-
-        }
-    }
-
     [QuickFix]
     public class MockedClassCreateFix : QuickFixBase
     {
@@ -104,7 +44,7 @@ namespace Tollrech
                 if (!ctorParam.Type.IsInterfaceType())
                     continue;
 
-                var argExpression = factory.CreateExpression($"NewMock<$0>();", ctorParam.Type);
+                var argExpression = factory.CreateExpression("NewMock<$0>();", ctorParam.Type);
 
                 if (classDeclaration != null)
                 {
@@ -115,7 +55,7 @@ namespace Tollrech
             }
 
             var names = ctorParams.Select(x => x.Type.IsInterfaceType() ? x.ShortName : "TODO");
-            var argumentsPattern = string.Join(", ", Enumerable.Range(1, ctorParams.Count).Select(x => string.Format("${0}", x)));
+            var argumentsPattern = string.Join(", ", Enumerable.Range(1, ctorParams.Count).Select(x => $"${x}"));
             var objArgExpressions = new object[] { ctor.GetContainingType() }.Concat(names).ToArray();
             var newExpression = factory.CreateExpression($"new $0({argumentsPattern});", objArgExpressions);
 
@@ -132,12 +72,12 @@ namespace Tollrech
                 return false;
 
             var ctorTreeNode = error?.Reference.GetTreeNode();
-            var classDeclaration = ctorTreeNode?.FindParent<IClassDeclaration>();
+            var classDeclaration = ctorTreeNode.FindParent<IClassDeclaration>();
             if (classDeclaration == null)
                 return false;
-            return true;
-            var methods = classDeclaration.SuperTypes.SelectMany(x => x.GetTypeElement()?.Methods);
-            return methods.Any(x => x.ShortName == "NewMock");
+
+            var superTypes = classDeclaration.SuperTypes.SelectMany(x => x.GetAllSuperTypes()).Concat(classDeclaration.SuperTypes);
+            return superTypes.Any(x => x.GetClassType()?.Methods.Any(y => y.ShortName == "NewMock") ?? false);
         }
     }
 }
