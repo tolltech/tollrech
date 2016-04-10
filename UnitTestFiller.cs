fa93f360eec0cd8465a6efc04cbab08f523a5799
tolltech <alexandrovpe@gmail.com>
2016-04-10 22:09:18 +0400

    unit test filler dummy

---
+++ UnitTestFiller.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using JetBrains.Application.Progress;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Feature.Services.ContextActions;
+using JetBrains.ReSharper.Feature.Services.CSharp.Analyses.Bulbs;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+using JetBrains.TextControl;
+using JetBrains.Util;
+
+namespace Tollrech
+{
+    [ContextAction(Group = "C#", Name = "Unit Test Action", Description = "Fill unit tests' mocks")]
+    public class UnitTestFiller : ContextActionBase
+    {
+        private ICSharpContextActionDataProvider Provider { get; set; }
+
+        public UnitTestFiller(ICSharpContextActionDataProvider provider)
+        {
+            Provider = provider;
+        }
+
+        protected override Action<ITextControl> ExecutePsiTransaction(ISolution solution, IProgressIndicator progress)
+        {
+            Work();
+            return null;
+        }
+
+        public override string Text { get; } = "Fill unit tests' mocks";
+
+        public override bool IsAvailable(IUserDataHolder cache)
+        {
+            var method = Provider.GetSelectedElement<IMethodDeclaration>();
+
+            var insideOfMethod = method != null;
+
+            if (insideOfMethod)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private void Work()
+        {
+            //IMethodDeclaration method = Provider.GetSelectedElement<IMethodDeclaration>();
+
+            //IType type = method.DeclaredElement.ReturnType;
+
+            //string typePresentableName = type.GetPresentableName(CSharpLanguage.Instance);
+
+            //CSharpElementFactory factory = CSharpElementFactory.GetInstance(Provider.PsiModule);
+
+            //string code = $"new {typePresentableName}()";
+
+            //ICSharpExpression newExpression = factory.CreateExpression(code);
+
+            //IReturnStatement returnStatement = Provider.GetSelectedElement<IReturnStatement>(false);
+
+            //returnStatement.SetValue(newExpression);
+        }
+    }
+}
