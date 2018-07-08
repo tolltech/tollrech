a6b51406b3b5f468c69c8c618824e0fcaa88ba4a
tolltech <alexandrovpe@gmail.com>
2018-07-08 14:34:32 +0400

    i ve tried so hard

---
+++ Tollrech/EFClass/SqlMapGeneratorContextAction.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using JetBrains.Application.Progress;
+using JetBrains.Metadata.Reader.Impl;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Feature.Services.ContextActions;
+using JetBrains.ReSharper.Feature.Services.CSharp.Analyses.Bulbs;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.CSharp;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+using JetBrains.TextControl;
+using JetBrains.Util;
+
+namespace Tollrech.EFClass
+{
+    [ContextAction(Name = "SqlMapGenerate", Description = "Generate Sql map for class-entity", Group = "C#", Disabled = false, Priority = 1)]
+    public class SqlMapGeneratorContextAction : ContextActionBase
+    {
+        private readonly ICSharpContextActionDataProvider provider;
+        private readonly IClassDeclaration classDeclaration;
+
+        public SqlMapGeneratorContextAction(ICSharpContextActionDataProvider provider)
+        {
+            this.provider = provider;
+            classDeclaration = provider.GetSelectedElement<IClassDeclaration>();
+        }
+
+        protected override Action<ITextControl> ExecutePsiTransaction(ISolution solution, IProgressIndicator progress)
+        {
+            var factory = provider.ElementFactory;
+
+            provider.PsiModule.Get
+
+            ITypeElement typeElement = provider.PsiServices
+                .Caches
+                .
+                .GetDeclarationsCache(psiModule, true, false)
+                .GetTypeElementsByCLRName(new ClrTypeName("System.Runtime.Serialization.DataMemberAttribute"))
+                .First();
+
+            foreach (var propertyDeclaration in classDeclaration.PropertyDeclarations)
+            {
+                factory.CreateAttribute(factory.Cre)
+
+                propertyDeclaration.AddAttributeBefore(new);
+            }
+
+
+            return null;
+        }
+
+        public override string Text => "Add EF Sql map";
+
+        public override bool IsAvailable(IUserDataHolder cache)
+        {
+            return classDeclaration != null;
+        }
+    }
+}
