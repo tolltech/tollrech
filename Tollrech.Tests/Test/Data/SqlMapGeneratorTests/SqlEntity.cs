908ee774e8d35c6a977921c0575add4bdd63e04f
tolltech <alexandrovpe@gmail.com>
2018-07-14 13:03:46 +0400

    workarounf for sql test

--- Tollrech.Tests/Test/Data/SqlMapGeneratorTests/SqlEntity.cs
+++ Tollrech.Tests/Test/Data/SqlMapGeneratorTests/SqlEntity.cs
@@ -1,5 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 
+namespace System.ComponentModel.DataAnnotations.Schema
+{
+    public class TableAttribute : Attribute
+    {
+
+    }
+}
+
 namespace Tollrech.Tests.Test.Data.SqlMapGeneratorTests
 {
     public class SqlEntity{caret}
