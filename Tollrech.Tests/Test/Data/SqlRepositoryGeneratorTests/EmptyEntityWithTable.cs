896d3b59a2a13e04d92ddef4db281bb9e8bd3779
tolltech <alexandrovpe@gmail.com>
2021-05-12 15:37:39 +0500

    add availabilty of repo via table attribute

---
+++ Tollrech.Tests/Test/Data/SqlRepositoryGeneratorTests/EmptyEntityWithTable.cs
@@ -0,0 +1,11 @@
+﻿namespace Tollrech.Tests.Test.Data.SqlMapGeneratorTests
+{
+    [Table("InvoiceTransactions")]
+    public class EmptyEntity{caret}
+    {
+        public int Field;
+        public int PropGetField => 42;
+        public int G { get; }
+        public int S { set; }
+    }
+}
